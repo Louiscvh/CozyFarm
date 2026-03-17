64541e50a4ec6d3f383c9e3d4ebc6065455f7c0a
Louis Cavecchi <cavecchilouis@gmail.com>
2026-03-18 00:19:43 +0100

    Fix trees size + add market

--- src/game/entity/entities/Tree1.ts
+++ src/game/entity/entities/Tree1.ts
@@ -4,5 +4,5 @@ export const Tree1Entity: Entity = {
   id: "tree1",
   model: "/models/tree_1.glb",
   modelSize: 4,
-  footprint: 1
+  footprint: 2
 }
--- src/game/entity/entities/TreeOrange.ts
+++ src/game/entity/entities/TreeOrange.ts
@@ -4,5 +4,5 @@ export const TreeOrangeEntity: Entity = {
   id: "tree_orange",
   model: "/models/tree_orange.glb",
   modelSize: 3,
-  footprint: 2
+  footprint: 1
 }
